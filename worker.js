3e215703d2138667bb81b80b2e918589b0a25c9b
Rob Henry <roberthenry@email.com>
2021-08-08 02:21:57 -0500

    beginning to cache assets. working toward offline

--- worker.js
+++ worker.js
@@ -1,14 +1,40 @@
+const staticCacheName = "site-static";
+const assets = [
+  "/",
+  "/index.html",
+  "/js/app.js",
+  "/js/ui.js",
+  "/js/materialize.min.js",
+  "/css/styles.css",
+  "/css/materialize.min.css",
+  "/img/dish.png",
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
+  "https://fonts.gstatic.com/s/materialicons/v97/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2"
+];
+
 // install event
 self.addEventListener("install", event => {
-  console.log("service worker has been installed!");
+  // waituntil keeps the worker running until it's done caching (you don't want it to quit prematurely)
+  event.waitUntil(
+    // this opens the existing or creates a new cache with the provided name
+    caches.open(staticCacheName).then(cache => {
+      console.log("caching shell assets");
+      // adds all of the files listed in the array to the cache
+      cache.addAll(assets);
+    })
+  );
 });
 
 // activate event
 self.addEventListener("activate", event => {
-  console.log("service worker has been activated");
+  //console.log("service worker has been activated");
 });
 
 // fetch event
 self.addEventListener("fetch", event => {
-  console.log("fetch event", event);
+  event.respondWith(
+    caches.match(event.request).then(cachedresponse => {
+      return cachedresponse || fetch(event.request);
+    })
+  );
 });
