7ba092c091f92eeed5e6682e42230dd9004bf85d
Rob Henry <roberthenry82@hotmail.com>
2021-08-09 15:33:52 -0500

    Now rendering actual data to the screen and enabled some offline data abilities

--- js/db.js
+++ js/db.js
@@ -1,9 +1,21 @@
+// offline data
+db.enablePersistence()
+	.catch(error => {
+		if (error.code == "failed-precondition") {
+			// probably multiple tabs
+			console.log("persistence failed");
+		} else if(error.code == "unimplemented") {
+			// browser doesn't support
+			console.log("persistence is not available");
+		}
+	});
+
 // realtime listener
 db.collection("recipes").onSnapshot(snapshot => {
 	snapshot.docChanges().forEach(change => {
-		console.log("change", change.doc.data(), change.doc.id);
 		if (change.type === "added") {
 			// add doc to page
+			renderrecipe(change.doc.data(), change.doc.id);
 		}
 		if (change.type === "removed") {
 			// remove doc from page
